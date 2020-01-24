5f74dd4ce75ad4c784fc3382c4ebf805182707d7
Reuben <4957964+reubn@users.noreply.github.com>
2020-01-24 22:56:03 +0000

    Scale

--- src/components/DayTimeline/Period/index.js
+++ src/components/DayTimeline/Period/index.js
@@ -3,15 +3,23 @@ import {View, Text} from 'react-native'
 
 import {differenceInHours} from 'date-fns'
 
-export default ({event: {start, end, category, title}}) => {
+export default ({style: externalStyle, event: {start, end, category, title}}) => {
   const length = differenceInHours(end, start)
   // console.log({category, start, end, length})
   const style = [{
-    height: length * 100,
+    // position: 'absolute',
+    top: 0,
+    left: 0,
+    height: (length * 100) - 7,
     width: '100%',
-    backgroundColor: '#'+Math.floor(Math.random()*16777215).toString(16)
+    backgroundColor: '#'+Math.floor(Math.random()*16777215).toString(16),
+    borderRadius: 12,
+    // marginBottom: 15,
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center'
   }]
-  return <View style={style}>
+  return <View style={[externalStyle, style]}>
     <Text>{title}</Text>
     <Text>{category}</Text>
     <Text>{title}</Text>
--- src/components/DayTimeline/index.js
+++ src/components/DayTimeline/index.js
@@ -1,9 +1,9 @@
 import React, {useState} from 'react'
-import {Text} from 'react-native'
+import {View, Text} from 'react-native'
 
 import Period from './Period'
 
-import {compareAsc, isEqual} from 'date-fns'
+import {compareAsc, isEqual, differenceInHours} from 'date-fns'
 
 export default ({events}) => {
   const [data, setData] = useState(events)
@@ -16,10 +16,29 @@ export default ({events}) => {
       : undefined
 
     return [...array, ...(freeTime ? [freeTime, event] : [event])]
-  }, []).map((event, i) => <Period key={event.id || `free-${i}`} event={event} />)
-  return periods
+  }, [])
+
+  const elements = periods.map((event, i, array) => <Period key={event.id || `free-${i}`} event={event}  />)
+
+  const hours = Math.ceil(differenceInHours(periods[periods.length - 1].end, periods[0].start))
+
+  return (
+    <View style={{flex: 1, alignItems: 'flex-start', justifyContent: 'flex-start', flexDirection: 'row'}}>
+    <View style={{height: differenceInHours(periods[periods.length - 1].end, periods[0].start) * 100, width: '25%', backgroundColor: 'green'}}>
+      {Array.from({length: hours}, (_, i) => <View key={`scale-${i}`} style={{overflow: 'visible', height: 100, backgroundColor: '#'+Math.floor(Math.random()*16777215).toString(16)}}>
+        <Text style={{fontSize: 16, position: 'relative', top: -10}}>10:00</Text>
+        <View style={{height:2, width: 10, backgroundColor: 'black', position:'absolute', top: '25%'}}/>
+        <View style={{height:2, width: 20, backgroundColor: 'black', position:'absolute', top: '50%'}}/>
+        <View style={{height:2, width: 10, backgroundColor: 'black', position:'absolute', top: '75%'}}/>
+        {i == hours - 1 && <Text style={{fontSize: 16, position: 'relative', position:'absolute', top: '100%', transform:[{translateY: -10}]}}>10:00</Text>}
+      </View>)}
+    </View>
+    <View style={{width: '75%'}}>{elements}</View>
+    </View>
+  )
 }
 
 
 // <Period key={id} event={event} />
 // <Period key={Math.random().toString()} event={{category: 'free', start: array[array.length - 1].end, end: start}} />
+// style={{top: differenceInHours(event.start, array[0].start) * 70}}
